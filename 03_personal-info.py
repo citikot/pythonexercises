b1e95eb524370456f655cf0610a0520ca0ee35b8
Alex Pishvanov <citikot@dr.com>
2022-07-20 18:28:14 +0300

    task 03 added

---
+++ 03_personal-info.py
@@ -0,0 +1,8 @@
+firstname = input("What is your first name? ")
+lastname = input("What is your last name? ")
+age = input("What is your age? ")
+location = input("What is your location? ")
+
+print(f'The type of age is {type(age)}')
+
+print(f'Hello, {firstname} {lastname}! Your age is {age} and location {location}.')
